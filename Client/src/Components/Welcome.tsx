a0932b1409f592eb671b566029df68c3a2a30e44
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-27 20:52:22 +1000

    started on smart contract implementatino

--- Client/src/Components/Welcome.tsx
+++ Client/src/Components/Welcome.tsx
@@ -27,8 +27,8 @@ export default function Navbar() {
     const handleSubmit = () => {}
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> Accross The World
           </h1>
@@ -60,7 +60,7 @@ export default function Navbar() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -69,7 +69,7 @@ export default function Navbar() {
                 </div>
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
-              <div>
+              <div> 
                 <p className="text-white font-light text-sm">
                   Placeholder address
                 </p>
