ceb033f5bbb1921d7ebf536cb138975a64013f1a
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-08-27 12:26:33 +1000

    added packages, finished the navbar component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,6 @@
 import Navbar from './Components/Navbar';
 import Welcome from "./Components/Welcome";
 import Services from "./Components/Services";
-import Loader from "./Components/Loader";
 import Transactions from './Components/Transactions';
 import Footer from "./Components/Footer";
 
